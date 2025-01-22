3219ebe4089eb824ddfd3c0f26e453d7e152f058
xakxa01 <Carlosxaviergomez@outlook.com>
2025-01-21 23:57:34 -0400

    refactor: switch to memory storage for file uploads and simplify attachment handling

--- index.js
+++ index.js
@@ -2,33 +2,13 @@ import 'dotenv/config';
 import express from 'express';
 import multer from 'multer';
 import nodemailer from 'nodemailer';
-import path from 'path';
-import fs from 'fs';
 import morgan from 'morgan';
 import cors from 'cors';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-const uploadsDir = path.join(__dirname, 'uploads');
-if (!fs.existsSync(uploadsDir)) {
-	fs.mkdirSync(uploadsDir);
-	console.log(`Carpeta "uploads" creada.`);
-}
-
-const storage = multer.diskStorage({
-	destination: (req, file, cb) => {
-		cb(null, 'uploads/');
-	},
-	filename: (req, file, cb) => {
-		cb(null, Date.now() + path.extname(file.originalname));
-	},
-});
-
+const storage = multer.memoryStorage();
 const upload = multer({ storage });
 
 app.use(express.json());
@@ -51,28 +31,23 @@ app.get('/', (req, res) => {
 app.post('/send-email', upload.single('file'), async (req, res) => {
 	try {
 		const { errorType, message, ruta } = req.body;
-		let attachment = null;
-
-		if (req.file) {
-			attachment = {
-				filename: req.file.filename,
-				path: path.join(__dirname, 'uploads', req.file.filename),
-			};
-		}
 
 		const mailOptions = {
 			from: process.env.EMAIL_USER,
 			to: process.env.RECIPIENT_EMAIL,
 			subject: `Reporte de ${errorType}`,
 			text: `Mensaje: ${message}${ruta ? `\nRuta: ${ruta}` : ''}`,
-			attachments: attachment ? [attachment] : [],
+			attachments: req.file
+				? [
+					{
+						filename: req.file.originalname,
+						content: req.file.buffer,
+					},
+				]
+				: [],
 		};
 
 		const info = await transporter.sendMail(mailOptions);
-
-		if (req.file) {
-			fs.unlinkSync(path.join(__dirname, 'uploads', req.file.filename));
-		}
 
 		res.status(200).json({ message: 'Reporte enviado correctamente', info });
 	} catch (error) {
